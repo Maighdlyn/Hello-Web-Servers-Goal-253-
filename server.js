af1b9020efb1cb385a9da7c9b0e937ffc055e3c1
Maighdlyn <emmamaighdlyn@gmail.com>
2017-05-16 12:01:34 -0700

    Set up placeholder files

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
 var express = require('express')
 var app = express()
+var path = require('path')
 
 app.listen(3900, function(req, res){
   console.log('Example app listening on port 3900')
+})
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+app.get('/artist/:artist_id', (req,res) => {
+  res.render('artist')
+})
+app.get('/albums', (req, res) => {
+  res.render('albums')
+})
+app.get('/albums/:album_id', (req, res) =>{
+  res.render('album')
+})
+app.get('/songs', (req, res) => {
+  res.render('songs')
 })
