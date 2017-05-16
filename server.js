9e98bdb214dc97fd0d0df1b1a9bbde02ff7244b1
Maighdlyn <emmamaighdlyn@gmail.com>
2017-05-16 15:05:33 -0700

    added stylesheet

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app.listen(3900, function(req, res){
 })
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
+app.use('/', express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   res.render('index')
